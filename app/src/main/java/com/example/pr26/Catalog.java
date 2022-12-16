ff9d41d63b625055723aeef11946d05db270afa5
LaironLF <laironlf@gmail.com>
2022-12-16 15:30:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pr26/Catalog.java
@@ -0,0 +1,17 @@
+package com.example.pr26;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Catalog extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_catalog);
+    }
+}
